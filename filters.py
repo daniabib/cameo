e5ddbdafb7827eb05dc32d2205f91303aa6a4118
daniabib <danielsabib@gmail.com>
2021-05-31 10:48:32 -0300

    End Ch 2

---
+++ filters.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import utils
+
+def strokeEdges(src, dst, blurKsize=7, edgeKsize=5):
+    if blurKsize >= 3:
+        blurredSrc = cv2.medianBlur(src, blurKsize)
+        graySrc = cv2.cvtColor(blurredSrc, cv2.COLOR_BGR2GRAY)
+    else:
+        graySrc = cv2.cvtColor(src, cv2.COLOR_BGRGRAY)
+        
+    cv2.Laplacian(graySrc, cv2.CV_8, graySrc, ksize=edgeKsize)
+    normalizedInverseAlpha = (1.0 / 255) * (255 - graySrc)
+    channels = cv2.split(src)
+    for channel in channels:
+        channel[:] = channel * normalizedInverseAlpha
+    cv2.merge(channels, dst)
+    
+
+
